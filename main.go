eaf3d808a2fa3755e7a9359c8fcdad771ee811c8
NopFault <nopfault@NopFaults-MacBook-Air.local>
2023-08-25 15:03:22 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,69 @@
+package lzsigma
+
+import (
+	"io"
+	"net/http"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Logsource struct {
+	Product string
+	Service string
+}
+type Rule struct {
+	Title          string
+	Id             string
+	Status         string
+	Description    string
+	Preferences    []string
+	Author         string
+	Date           string
+	Modified       string
+	Tags           []string
+	Logsource      Logsource
+	Detection      map[string]interface{}
+	Falsepositives []string
+	Level          string
+}
+
+type SigmaRule struct {
+	source string
+}
+
+func (sr *SigmaRule) content() []byte {
+
+	data, err := http.Get(sr.source)
+	if err != nil {
+
+		f, err := os.Open(sr.source)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		cont, err := io.ReadAll(f)
+		if err != nil {
+			panic(err)
+		}
+
+		return cont
+	}
+	defer data.Body.Close()
+
+	data_bytes, err := io.ReadAll(data.Body)
+	if err != nil {
+		panic(err)
+	}
+	return data_bytes
+}
+
+func (sr *SigmaRule) Parse() Rule {
+	var rule Rule
+	if err := yaml.Unmarshal(sr.content(), &rule); err != nil {
+		panic(err)
+	}
+
+	return rule
+}
